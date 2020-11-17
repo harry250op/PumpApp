4ad90f63293949ce4e68fb23ea827baca9401c60
AdamPrzywuski <adamprzywuski@gmail.com>
2020-11-17 01:20:14 +0100

    Added getting information from user

--- app/src/main/java/com/example/pumpapp/MainActivity.kt
+++ app/src/main/java/com/example/pumpapp/MainActivity.kt
@@ -14,26 +14,43 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
 
         val databaseUser=baseContext.openOrCreateDatabase("user_info", MODE_PRIVATE,null)
         try{
-            val test="SELECT * from user"
-            databaseUser?.execSQL(test)
+            val test="SELECT * from user "
+            databaseUser.rawQuery(test,null)
+
         }catch ( e:Exception)
         {
+            Log.d(TAG, e.toString())
             //database dont exist
-            val sqlcreatingtable1="CREATE TABLE user(_id INTEGER PRIMARY KEY NOT NULL, name TEXT,age INT,sex TEXT,weight INT,height INT)"
+            val sqlcreatingtable1="CREATE TABLE user (_id INTEGER PRIMARY KEY NOT NULL, name TEXT,age TEXT,sex TEXT,weight TEXT,height TEXT)"
             databaseUser?.execSQL(sqlcreatingtable1)
                 Log.d(TAG, "The database exist but it's empty")
+
                 val intent= Intent(this,RegistrationActivity::class.java)
                 startActivityForResult(intent,1)
 
         }
+        Log.d(TAG,"horse")
+
 
 
         }
 
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            if (resultCode == AppCompatActivity.RESULT_OK) {
+                Log.d(TAG,"The user has sent the data")
 
+
+
+            }
+        }
     }
+    }
+
 
--- app/src/main/java/com/example/pumpapp/RegistrationActivity.kt
+++ app/src/main/java/com/example/pumpapp/RegistrationActivity.kt
@@ -1,26 +1,64 @@
 package com.example.pumpapp
 
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
-    private val TAG="registrationActivity"
+    private const val TAG="registrationActivity"
 
 
 class RegistrationActivity: AppCompatActivity()  {
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val databaseUser=baseContext.openOrCreateDatabase("user_info", MODE_PRIVATE, null)
         setContentView(R.layout.activity_getting_data_user)
         var editName: EditText? =findViewById(R.id.editTextName)
-        var editAge: EditText? =findViewById(R.id.editTextAge)
-        var editWeight: EditText? =findViewById(R.id.editTextNumberWeight)
-        var editHeight: EditText? =findViewById(R.id.editTextNumberHeight)
-        var button:Button=findViewById<Button>(R.id.button1)
+        val editAge: EditText? =findViewById(R.id.editTextAge)
+        val editWeight: EditText? =findViewById(R.id.editTextNumberWeight)
+        val editHeight: EditText? =findViewById(R.id.editTextNumberHeight)
+        val button:Button=findViewById<Button>(R.id.button1)
+        val checkBoxMen=findViewById<CheckBox>(R.id.checkBoxMale)
+        var checkBoxWomen=findViewById<CheckBox>(R.id.checkBoxFemale)
 
 
 
-        button.setOnClickListener {}
+
+
+        button.setOnClickListener {
+            var sex=""
+            val user = ContentValues()
+            Log.d(TAG, editName.toString())
+
+            if(checkBoxMen.isChecked)
+            {
+                sex="man"
+            }else sex="woman"
+
+
+            user.put("name", editName?.text.toString())
+            user.put("age", editAge?.text.toString())
+            user.put("sex",sex)
+            user.put("weight", editWeight?.text.toString())
+            databaseUser.insert(
+                "user",
+                null,
+                user
+            )
+
+            Log.d(TAG, "The data has been added to database")
+            val test="SELECT * from user "
+            databaseUser.execSQL(test)
+            val data= Intent()
+            setResult(RESULT_OK,data)
+            finish()
+
+        }
 
     }
 
