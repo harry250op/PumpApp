a5bb94e8f9515c6edd405200eb4dd470146215a1
AdamPrzywuski <adamprzywuski98@gmail.com>
2020-12-28 23:50:55 +0100

    Added views and theirs logic

--- app/src/main/java/com/example/pumpapp/Creating_training_adapter.kt
+++ app/src/main/java/com/example/pumpapp/Creating_training_adapter.kt
@@ -1,5 +1,6 @@
 package com.example.pumpapp
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,16 +12,21 @@ import androidx.recyclerview.widget.RecyclerView
 
 class Creating_training_adapter(val dataSet: ArrayList<Excercise>) : RecyclerView.Adapter<Creating_training_adapter.ViewHolder>()
 {
+    var time=0
+    var reps=0
 
     class ViewHolder(v: View):RecyclerView.ViewHolder(v) {
 
-        val nameExcercise: TextView = v.findViewById<TextView>(R.id.textViewNameOfExcercise)
+        val nameExcercise = v.findViewById<TextView>(R.id.textViewNameOfExcercise)
         val typeExcercise=v.findViewById<TextView>(R.id.textViewType)
+        val reps=v.findViewById<TextView>(R.id.textViewReps)
+        val time=v.findViewById<TextView>(R.id.textViewTime)
+        val plusReps=v.findViewById<Button>(R.id.buttonAddingReps)
+        val minusReps=v.findViewById<Button>(R.id.buttonDeletingReps)
+        val plusTime=v.findViewById<Button>(R.id.buttonAddingTime)
+        val minusTime=v.findViewById<Button>(R.id.buttonDeletingTime)
+        val addExcercise=v.findViewById<Button>(R.id.ButtonAddingExcercise)
         var context=v.getContext()
-
-
-
-
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -30,9 +36,36 @@ class Creating_training_adapter(val dataSet: ArrayList<Excercise>) : RecyclerVie
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: Creating_training_adapter.ViewHolder, position: Int) {
         val dataFromDatabase:Excercise=dataSet[position]
         holder.nameExcercise.text = dataFromDatabase.name_of_excercise
+        holder.typeExcercise.text=dataFromDatabase.type_of_excercise
+        holder.setIsRecyclable(false)
+        holder.plusReps.setOnClickListener()
+        {
+            reps+=1
+            holder.reps.text= "Reps: $reps"
+        }
+        holder.minusReps.setOnClickListener()
+        {
+            reps-=1
+            holder.reps.text= "Reps: $reps"
+        }
+        holder.plusTime.setOnClickListener()
+        {
+            time+=30
+            holder.time.text= "Time: $time"
+        }
+        holder.minusTime.setOnClickListener()
+        {
+            time-=30
+            holder.time.text= "Time: $time"
+        }
+        holder.addExcercise.setOnClickListener()
+        {
+            
+        }
     }
 
     override fun getItemCount(): Int {
